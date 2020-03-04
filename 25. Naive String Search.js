276be8702bf974c9b162a16aa2f9e9c36ae987ba
Tanya Singh <tanyacs27@gmail.com>
2020-03-04 23:18:54 +0530

    Find count of occurences of short string in long string

---
+++ 25. Naive String Search.js
@@ -0,0 +1,12 @@
+function naiveSearch(longStr, shortStr){
+   let count =0;
+   for(let i=0; i<longStr.length; i++){
+       for(let j=0; j<shortStr.length; j++){
+           if(longStr[i+j] !== shortStr[j]) break;
+           if(j === shortStr.length - 1) count++;
+       }
+   }
+    return count;
+}
+
+console.log(naiveSearch("lorie loled", "lol")); 
